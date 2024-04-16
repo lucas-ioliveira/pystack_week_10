749717bcab1cf7709a73796339947f0071760023
Lucas Oliveira <lucasio2008@gmail.com>
2024-04-15 23:46:29 -0300

    feat(healing_project/usuarios):Implemetação das rotas de login e logout.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from usuarios import views
 
-urlpatterns = [path("cadastro/", views.cadastro, name="cadastro")]
+urlpatterns = [
+    path("cadastro/", views.cadastro, name="cadastro"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout, name='sair')
+]
