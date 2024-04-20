c3492431168ab9d147c7193e46ba1a61c57ba0f2
Lucas Oliveira <lucasio2008@gmail.com>
2024-04-19 22:40:29 -0300

    feat(healing_project/medico):
    Adiciona ao admin datas abertas.

--- medico/admin.py
+++ medico/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from medico.models import Especialidades, DadosMedico
+from medico.models import Especialidades, DadosMedico, DatasAbertas
 
 
 admin.site.register(Especialidades)
 admin.site.register(DadosMedico)
+admin.site.register(DatasAbertas)
