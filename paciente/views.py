f392130ad02b5103131d8ce994262fec56a276be
Lucas Oliveira <lucasio2008@gmail.com>
2024-04-21 12:28:53 -0300

    feat(healing_paciente): Minhas Consultas.
    Lista todas as consultas agendadas do paciente.

--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import constants
+from datetime import datetime
 
 from medico.models import DadosMedico, Especialidades, DatasAbertas
+from paciente.models import Consulta
 
-from datetime import datetime
+
 
 def home(request):
     if request.method == "GET":
@@ -26,3 +30,31 @@ def escolher_horario(request, id_dados_medicos):
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
         return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == "GET":
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+
+        horario_agendado = Consulta(
+            paciente=request.user,
+            data_aberta=data_aberta
+        )
+
+        horario_agendado.save()
+
+        # TODO: Sugestão Tornar atomico
+
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Horário agendado com sucesso.')
+
+        return redirect('/pacientes/minhas_consultas/')
+
+
+def minhas_consultas(request):
+    if request.method == "GET":
+        #TODO: desenvolver filtros
+        minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
