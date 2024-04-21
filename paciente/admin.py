905fca0aef298151a50ca838628e4653900aa165
Lucas Oliveira <lucasio2008@gmail.com>
2024-04-21 16:23:53 -0300

    feat(healing_project/paciente): Model documentos
    Desenvolvimento do model documentos

--- paciente/admin.py
+++ paciente/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from paciente.models import Consulta
+from paciente.models import Consulta, Documento
 
 
 admin.site.register(Consulta)
+admin.site.register(Documento)
